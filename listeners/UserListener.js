e06c36498e245082e48295d0d388b728487747de
zThundy <antonioantonelli7@gmail.com>
2022-10-01 22:20:05 +0200

    moved user join role to UserListener

--- listeners/UserListener.js
+++ listeners/UserListener.js
@@ -5,6 +5,24 @@ class Listener {
     }
 
     run(event, client, user) {
+        switch(event) {
+            case "guildMemberAdd":
+                this.guildMemberAdd(client, user);
+                break;
+            case "guildMemberRemove":
+                this.guildMemberRemove(client, user);
+                break;
+            case "guildMemberUpdate":
+                this.guildMemberUpdate(client, user);
+                break;
+        }
+    }
+
+    guildMemberAdd(client, user) {
+        // find specific role and add to user
+        const role = message.guild.roles.cache.find(role => role.id === this.config.roleAssiger.userRole);
+        // assign that role to the user
+        user.roles.add(role);
     }
 }
 
